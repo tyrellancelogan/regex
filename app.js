af61cf5d38fd0ce5c201bb3be03bda0a739059fd
tyrell <tyrellancelogan@gmail.com>
2021-11-22 20:36:34 +0100

    added event listener to inputs

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 // Regex validation object
 const validate = {
     username: /^([a-zA-z][\w]){5,12}$/,
-    email: /^([a-zA-z][\w\.-]+)@([a-zA-z\d]+)\.([a-z]{1,5})(?:\.([a-z]{1,5}))?$/,
+    email: /^([a-zA-z][\w\.-]+)@([a-zA-z\d-]+)\.([a-z]{1,5})(?:\.([a-z]{1,5}))?$/,
+    password: /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-_=]).{8,}$/,
+    phone: /^((0\d{3})[ ]?(\d{3})[ ]?(\d{4}))|(\+?\d{10,13})$/,
 };
+
+const formInputs = document.querySelectorAll("#signup input");
+
+for (input of formInputs) {
+    input.addEventListener("click", (e) => {
+        console.log(e);
+    });
+}
