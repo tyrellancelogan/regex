78315fbce730a8ec26a8ecb61acb0ed4b0c9b2e3
tyrell <tyrellancelogan@gmail.com>
2021-11-22 08:42:53 +0100

    added regex to validate obj

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-// regex validation object
-const validator = {
-    username: /[\w-]{5,8}/,
-    email: /[a-zA-Z\d-\.]+@[a-zA-Z\d-]+\.[a-zA-z]{3,10}(\.[a-zA-z]{3,10})?/,
+// Regex validation object
+const validate = {
+    username: /^([a-zA-z][\w]){5,12}$/,
+    email: /^([a-zA-z][\w\.-]+)@([a-zA-z\d]+)\.([a-z]{1,5})(?:\.([a-z]{1,5}))?$/,
 };
