4984127021360a88929730f93291912e8fcf4d07
tyrell <tyrellancelogan@gmail.com>
2021-11-23 14:14:20 +0100

    function to test input value with regex pattern, action based on test

--- app.js
+++ app.js
@@ -1,7 +1,29 @@
 // Regex validation object
 const validate = {
-    username: /^([a-zA-z][\w]){5,12}$/,
+    username: /^[a-zA-z][\w]{3,15}$/,
     email: /^([a-zA-z][\w\.-]+)@([a-zA-z\d-]+)\.([a-z]{1,5})(?:\.([a-z]{1,5}))?$/,
     password: /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-_=]).{8,}$/,
+    confirm: /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-_=]).{8,}$/,
     phone: /^((0\d{3})[ ]?(\d{3})[ ]?(\d{4}))|(\+?\d{10,13})$/,
 };
+
+// get form object and inputs objects
+const formObj = document.querySelector("#signup");
+const inputObj = document.querySelectorAll("#signup input");
+
+// test regex pattern with user input
+const inputValidate = (reg, input) => reg.test(input.value);
+
+// add event listener to inputs obj
+for (input of inputObj) {
+    input.addEventListener("input", (evt) => {
+        // console.dir(evt.target);
+        // consol.log("a");
+        // change class of input field based on regex test on input value
+        if (inputValidate(validate[evt.target.name], evt.target)) {
+            console.log(true);
+        } else {
+            console.log(false);
+        }
+    });
+}
